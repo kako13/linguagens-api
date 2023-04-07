ee86333cc10e7c3616dd597cdf263788197c9004
Kauê Oliveira <kso13@hotmail.com>
2023-04-07 05:53:01 -0300

    Aula - 4 - Desafio 1 CRUD

--- src/main/java/br/com/alura/linguagensapi/LinguagemController.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagemController.java
@@ -1,27 +1,60 @@
 package br.com.alura.linguagensapi;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/linguagens", produces = MediaType.APPLICATION_JSON_VALUE)
 public class LinguagemController {
 
-//    @Autowired
+    @Autowired
     private LinguagemRepository respository;
 
-    public LinguagemController(LinguagemRepository respository) {
-        this.respository = respository;
+
+    @GetMapping
+    public ResponseEntity<List<Linguagem>> listar() {
+        List<Linguagem> linguagens = respository.findAll();
+        linguagens.forEach(System.out::println);
+        return ResponseEntity.ok(linguagens);
     }
 
-    @GetMapping("/linguagens")
-    public List<Linguagem> obterLinguagens() {
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Linguagem adicionar(@RequestBody Linguagem linguagem) {
+        return respository.save(linguagem);
+    }
 
-        List<Linguagem> linguagens = respository.findAll();
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{linguagemId}")
+    public Linguagem atualizar(@PathVariable String linguagemId, @RequestBody Linguagem linguagem) {
 
-        linguagens.forEach(System.out::println);
-        return linguagens;
+        Optional<Linguagem> optional = respository.findById(linguagemId);
+        if (optional.isPresent()) {
+            Linguagem linguagemManaged = optional.get();
+            BeanUtils.copyProperties(linguagem, linguagemManaged);
+            return respository.save(linguagemManaged);
+        }
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{linguagemId}")
+    public Linguagem buscar(@PathVariable String linguagemId) {
+        return respository.findById(linguagemId).orElse(null);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{linguagemId}")
+    public void remover(@PathVariable String linguagemId) {
+        Optional<Linguagem> optional = respository.findById(linguagemId);
+        optional.ifPresent(linguagem -> respository.delete(linguagem));
+
     }
 }
