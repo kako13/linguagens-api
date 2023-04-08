dd15d8456ca8df6e099623315aaf18fc054717bb
Kauê Oliveira <kso13@hotmail.com>
2023-04-08 06:01:09 -0300

    Aula - 4 - Desafio 2 e 3. Ordenação no camada de persistencia

--- src/main/java/br/com/alura/linguagensapi/LinguagemRepository.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagemRepository.java
@@ -3,6 +3,9 @@ package br.com.alura.linguagensapi;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
+    List<Linguagem> findAllByOrderByRanking();
 }
