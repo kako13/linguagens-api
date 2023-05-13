e4860db4af93f8c8859e209b699a4ca0462299cc
Kauê Oliveira <kso13@hotmail.com>
2023-05-13 05:14:38 -0300

    OpenAPI - criando contexto, dependencia no pom e anotações de documentação

--- src/main/java/br/com/alura/linguagensapi/LinguagemController.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagemController.java
@@ -1,56 +1,90 @@
 package br.com.alura.linguagensapi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Tag(name = "linguagens")
 @RestController
-@RequestMapping(path = "/linguagens", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = {"/linguagens"}, produces = {"application/json"})
 public class LinguagemController {
-
     @Autowired
     private LinguagemRepository respository;
 
-
+    @Operation(summary = "Realiza a consulta das linguagens cadastradas", method = "GET")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Consulta relizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados da requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao consultar as linguagens")
+    })
     @GetMapping
     public ResponseEntity<List<Linguagem>> listar() {
-        List<Linguagem> linguagens = respository.findAllByOrderByRanking();
+        List<Linguagem> linguagens = this.respository.findAllByOrderByRanking();
         return ResponseEntity.ok(linguagens);
     }
 
+    @Operation(summary = "Realiza o cadastro de uma linguagem", method = "POST")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Cadastro relizado com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados da requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao cadastrar linguagem")
+    })
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
+    @PostMapping(consumes = {"application/json"})
     public ResponseEntity<Linguagem> adicionar(@RequestBody Linguagem linguagem) {
-        return new ResponseEntity<>(respository.save(linguagem), HttpStatus.CREATED);
+        return new ResponseEntity(this.respository.save(linguagem), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Realiza a alteração de uma linguagem cadastrada", method = "PUT")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Alteração relizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados da requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a alteração da linguagem")
+    })
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/{linguagemId}")
+    @PutMapping(value = {"/{linguagemId}"}, consumes = {"application/json"}, produces = {"application/json"})
     public Linguagem atualizar(@PathVariable String linguagemId, @RequestBody Linguagem linguagem) {
-        Linguagem linguagemManaged = respository.findById(linguagemId)
-                                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-            BeanUtils.copyProperties(linguagem, linguagemManaged);
-            return respository.save(linguagemManaged);
+        Linguagem linguagemManaged = (Linguagem)this.respository.findById(linguagemId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        BeanUtils.copyProperties(linguagem, linguagemManaged);
+        return (Linguagem)this.respository.save(linguagemManaged);
     }
 
+    @Operation(summary = "Realiza a consulta de uma linguagem cadastradas", method = "GET")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Consulta relizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados da requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a consulta da linguagem")
+    })
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/{linguagemId}")
+    @GetMapping(value = {"/{linguagemId}"}, produces = {"application/json"})
     public Linguagem obterPorId(@PathVariable String linguagemId) {
-        return respository.findById(linguagemId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return (Linguagem)this.respository.findById(linguagemId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(summary = "Realiza a exclusão de uma linguagem cadastradas", method = "DELETE")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Exclusão relizada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Dados da requisição inválidos"),
+            @ApiResponse(responseCode = "400", description = "Parâmetros inválidos"),
+            @ApiResponse(responseCode = "500", description = "Erro ao realizar a exclusão da linguagem")
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{linguagemId}")
+    @DeleteMapping({"/{linguagemId}"})
     public void remover(@PathVariable String linguagemId) {
-        Linguagem linguagem = respository.findById(linguagemId)
-                                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-        respository.delete(linguagem);
-
+        Linguagem linguagem = (Linguagem)this.respository.findById(linguagemId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        this.respository.delete(linguagem);
     }
 }
--- src/main/java/br/com/alura/linguagensapi/LinguagensApiApplication.java
+++ src/main/java/br/com/alura/linguagensapi/LinguagensApiApplication.java
@@ -1,13 +1,14 @@
 package br.com.alura.linguagensapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "Linguagens API", version = "1.0", description =  "Swagger Ranking linguagens JSON \n\n## Introduction  \nEste é um exemplo do Ranking de linguagens. \n\n## Especificação OpenAPI  \nEssa API é documentada no **formato OpenAPI**. O projeto foi **extendido** e utiliza as features deste [template](https://github.com/PaloAltoNetworks/docusaurus-template-openapi-docs) e do [Docusaurus](https://docusaurus.io). A dependência utilizada doi a `springdoc-openapi-starter-webmvc-ui` na `versão v2` (https://springdoc.org/v2/), conforme a recomendação da [documentação](https://springdoc.org/v2/), quando utilizado `spring-boot v3`. \n\nEncontre mais sobre o Swagger em [http://swagger.io](http://swagger.io) ou em [irc.freenode.net, #swagger](http://swagger.io/irc/). \n\n## Proposta  \nA proposta desta API é fornecer um ranking fictício customizável das melhores linguagens de programação. \n\nAdministre o ranking conforme seus critérios. \n\n\n\nSim! É simples mesmo!"))
 @SpringBootApplication
 public class LinguagensApiApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(LinguagensApiApplication.class, args);
+		SpringApplication.run(br.com.alura.linguagensapi.LinguagensApiApplication.class, args);
 	}
-
 }
